a2f763453de32f1e6771184ff5936c638fb6c857
bingryan <bingryan@outlook.com>
2021-05-31 22:34:52 +0800

    add Filter trait

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -1,1 +1,19 @@
+use std::any::Any;
+use crate::config::Settings;
+
 mod redis;
+
+
+pub trait Filter: Any + Send + Sync {
+    /// filter name
+    fn name(&self) -> &'static str;
+
+    /// load settings
+    fn from_settings(settings: &Settings) -> Box<dyn Filter>;
+
+    /// fingerprint
+    fn fingerprint(self) -> bool;
+
+    /// clear filter queue
+    fn clear(self) -> bool;
+}
