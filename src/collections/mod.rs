3020fe24aeac13ef46b55a5d41089abb6c4c9c3d
bingryan <bingryan@outlook.com>
2021-06-01 15:50:09 +0800

    add LifoQueue

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -3,8 +3,8 @@ use anyhow::{Result, Error};
 
 pub trait HasQueue<T: Clone> {
     fn push(&mut self, element: T) -> Result<T>;
-    fn pop(&mut self) -> Option<T>;
-    fn clear(&mut self);
+    fn pop(&mut self) -> Result<T>;
+    fn clear(&mut self)-> Result<T>;
     fn len(&mut self) -> usize;
 }
 
@@ -14,14 +14,15 @@ pub trait InputOutputSerialization<T: Clone> {
 }
 
 
+/// FifoQueue is FIFO Queue data structure by memory
 #[derive(Debug)]
-pub struct BufferQueue<T: Clone> {
+pub struct FifoQueue<T: Clone> {
     queue: Vec<T>,
     capacity: usize,
 }
 
-impl<T: Clone> BufferQueue<T> {
-    pub fn new(capacity: usize) -> BufferQueue<T> {
+impl<T: Clone> FifoQueue<T> {
+    pub fn new(capacity: usize) -> FifoQueue<T> {
         BufferQueue {
             queue: vec![],
             capacity,
@@ -33,20 +34,20 @@ impl<T: Clone> BufferQueue<T> {
     }
 }
 
-impl<T: Clone> HasQueue<T> for BufferQueue<T> {
+impl<T: Clone> HasQueue<T> for FifoQueue<T> {
     fn push(&mut self, element: T) -> Result<T> {
         if self.queue.len() < self.capacity {
             self.queue.push(val);
             Ok(element)
         } else {
-            Error::msg("BufferQueue is full")
+            Error::msg("FifoQueue is full")
         }
     }
 
     fn pop(&mut self) -> Result<T> {
         match self.queue.first() {
             Some(val) => val.clone(),
-            None => Error::msg("BufferQueue is empty")
+            None => Error::msg("FifoQueue is empty")
         }
     }
 
@@ -54,7 +55,61 @@ impl<T: Clone> HasQueue<T> for BufferQueue<T> {
         if self.queue.len() > 0 {
             Ok(self.queue.clear())
         } else {
-            Error::msg("BufferQueue is empty")
+            Error::msg("FifoQueue is empty")
+        }
+    }
+
+    fn len(&mut self) -> usize {
+        if self.queue.len() > 0 {
+            self.queue.len()
+        } else {
+            0
+        }
+    }
+}
+
+/// LifoQueue is FIFO Queue data structure by memory
+#[derive(Debug)]
+pub struct LifoQueue<T: Clone> {
+    queue: Vec<T>,
+    capacity: usize,
+}
+
+impl<T: Clone> LifoQueue<T> {
+    pub fn new(capacity: usize) -> LifoQueue<T> {
+        BufferQueue {
+            queue: vec![],
+            capacity,
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+}
+
+impl<T: Clone> HasQueue<T> for LifoQueue<T> {
+    fn push(&mut self, element: T) -> Result<T> {
+        if self.queue.len() < self.capacity {
+            self.queue.push(val);
+            Ok(element)
+        } else {
+            Error::msg("LifoQueue is full")
+        }
+    }
+
+    fn pop(&mut self) -> Result<T> {
+        match self.queue.pop() {
+            Some(val) => val.clone(),
+            None => Error::msg("LifoQueue is empty")
+        }
+    }
+
+    fn clear(&mut self) -> Result<T> {
+        if self.queue.len() > 0 {
+            Ok(self.queue.clear())
+        } else {
+            Error::msg("LifoQueue is empty")
         }
     }
 
